eab9327ad324277fd0aefa5a1a705ddfaf6a1e22
nceres <45498644+nceres@users.noreply.github.com>
2021-04-18 18:30:22 -0700

    HW 2 - UI Layouts
    Created responsive text field and button grid layout

--- app/src/main/java/com/natesoft/nateapp/MainActivity.java
+++ app/src/main/java/com/natesoft/nateapp/MainActivity.java
@@ -1,8 +1,9 @@
 package com.natesoft.nateapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // based on Android Fundamentals tutorial "Your first interactive UI"
+    public void sendConfirmation(View view) {
+        Toast toast = Toast.makeText(this, R.string.send_confirmation,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void showMessageOne(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_one,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void showMessageTwo(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_two,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void showMessageThree(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_three,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void showMessageFour(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_four,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
 }
